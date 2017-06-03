976385febd75162b035e84090f3f427c2a4ec697
Eduardo Orochena <mint.alchemist@gmail.com>
2017-06-03 19:16:20 -0400

    test11

--- influxdb/tests/__init__.py
+++ influxdb/tests/__init__.py
@@ -16,7 +16,7 @@ using_pypy = hasattr(sys, "pypy_version_info")
 skipIfPYpy = unittest.skipIf(using_pypy, "Skipping this test on pypy.")
 
 _skip_server_tests = os.environ.get(
-'INFLUXDB_PYTHON_SKIP_SERVER_TESTS',
-None) == 'True'
+    'INFLUXDB_PYTHON_SKIP_SERVER_TESTS',
+    None) == 'True'
 skipServerTests = unittest.skipIf(_skip_server_tests,
-                                    "Skipping server tests...")
+    "Skipping server tests...")
