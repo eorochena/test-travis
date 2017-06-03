53d7da3af8f6f76e1b7fc895fc6e151eacbcef3c
Eduardo Orochena <mint.alchemist@gmail.com>
2017-06-03 18:15:28 -0400

    test5

--- influxdb/__init__.py
+++ influxdb/__init__.py
@@ -9,13 +9,10 @@ from .client import InfluxDBClient
 from .dataframe_client import DataFrameClient
 from .helper import SeriesHelper
 
-import unittest as unittest2
-
 __all__ = [
     'InfluxDBClient',
     'DataFrameClient',
     'SeriesHelper',
-    'unittest2'
 ]
 
 
